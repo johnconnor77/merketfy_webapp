256a7a989fbaabcecadb0e026d335a08c7b25a12
johnconnor77 <jfcalleh@unal.edu.co>
2022-02-19 17:38:55 -0500

    Update for JSON dummy

--- merketfy_API.py
+++ merketfy_API.py
@@ -32,8 +32,35 @@ async def get_article_markets(article_info: ArticleInfo):
         :param article_info: Class that represent the correct structure for Article Search on Markets
         :return: Output for Article Information
     """
+    dummy_json = {
+            "article_name" : article_info.article_name,
+            "article_markets" : [
+                {
+                    "market_name":"Éxito",
+                    "article_name_market": "iPhone 12 Pro Max 128 GB Grafito",
+                    "market_article_url": "https://www.exito.com/iphone%2012?_q=iphone%2012&map=ft",
+                    "article_image_url": "",
+                    "article_price": "6.399.900"
+                 },
+                {
+                    "market_name": "Falabella",
+                    "article_name_market": "iPhone 12 64GB",
+                    "market_article_url": "https://www.falabella.com.co/falabella-co/category/cat3460941/iPhone?sred=iphone-12",
+                    "article_image_url": "",
+                    "article_price": "3.699.900"
+                },
+                {
+                    "market_name": "Alkosto",
+                    "article_name_market": "iPhone 12 ",
+                    "market_article_url": "https://www.alkosto.com/iphone12mini-256gb-rojo/p/194252017142",
+                    "article_image_url": "",
+                    "article_price": "4.499.000"
+                },
+            ]
 
-    return JSONResponse("")
+    }
+
+    return JSONResponse(dummy_json)
 
 
 @app.post("/api/market/filtered", summary="Information in certain markets for a given Article",
