1003c32177786edfc48f09d720ee4df17e65d4a2
jalondono <juanlondono151776@hotmail.com>
2022-03-27 13:25:08 -0500

    [Update] ... - Dropped comments from install.sh script

--- schemas/install.sh
+++ schemas/install.sh
@@ -8,6 +8,3 @@ databaseName=$4
 createDatabase $hostname $user $password $databaseName
 useDatabase $hostname $user $password $databaseName
 installScript $hostname $user $password $databaseName "./tables/*.sql"
-
-# export PGPASSWORD=$password
-# psql -h localhost -U macbook -d postgres -w -v "ON_ERROR_STOP=1" -c "CREATE DATABASE merketfy_dev;"
